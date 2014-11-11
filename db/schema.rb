efff342c79f11f20e48e2e9fc9c263c9b342c92f
vampire <winsandymyint@gmail.com>
2014-11-11 23:43:00 +0800

    Customize form of the Review's Form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109153326) do
+ActiveRecord::Schema.define(version: 20141111143814) do
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20141109153326) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "image"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
